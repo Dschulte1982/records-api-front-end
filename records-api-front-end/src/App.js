11d00f4ae7dc399b84ae3ead29af4158477845dc
Dustin Schulte <dschulte1982@hotmail.com>
2024-04-05 10:33:01 -0700

    ShowRecordScreen page and ShowRecord component initial setup and connected to API

--- records-api-front-end/src/App.js
+++ records-api-front-end/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import ListAllRecords from "./pages/ListAllRecords/index";
 import AddRecordScreen from "./pages/AddRecord/index";
+import ShowRecordScreen from "./pages/ShowRecordScreen/index";
 import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<ListAllRecords />} />
+        <Route path="/:id" element={<ShowRecordScreen />} />
         <Route path="/add-record" element={<AddRecordScreen />} />
       </Routes>
     </div>
