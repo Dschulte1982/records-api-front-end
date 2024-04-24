0078fe4e84f37b41e5ed8fdc246ea60ea1459d8e
Dustin Schulte <dschulte1982@hotmail.com>
2024-04-24 15:19:40 -0700

    Added confirmation to delete functionality in CRUDButtons component

--- records-api-front-end/src/components/CRUDButtons/CRUDButtons.jsx
+++ records-api-front-end/src/components/CRUDButtons/CRUDButtons.jsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 import Stack from "react-bootstrap/Stack";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { RecordsAPI } from "../../apis/RecordsAPI";
 import "./styles.css";
 
-export const CRUDButtons = ({ id }) => {
+export const CRUDButtons = ({ id, name }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   const navigate = useNavigate();
+
   const handleEditClick = () => {
-    console.log("Edit was clicked");
+    navigate(`/${id}`);
   };
 
   const handleDeleteClick = (id) => {
     RecordsAPI.delete(id).then(() => {
-      alert(`Record with Id: ${id} was deleted`);
+      setShow(false);
       navigate(`/`);
     });
   };
@@ -34,12 +42,26 @@ export const CRUDButtons = ({ id }) => {
         fixedWidth
       />
       <FontAwesomeIcon
-        onClick={() => handleDeleteClick(id)}
+        onClick={handleShow}
         className="detailed-delete-icon"
         icon="fa-regular fa-trash-can"
         size="2x"
         fixedWidth
       />
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Item: {name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to delete this item?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Return
+          </Button>
+          <Button variant="danger" onClick={() => handleDeleteClick(id)}>
+            Confirm Deletion
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Stack>
   );
 };
