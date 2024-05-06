3446ad4551024a59e22e1a6fea3ffa141cbb8c74
Dustin Schulte <dschulte1982@hotmail.com>
2024-05-06 15:02:24 -0700

    Added Formik and Yup to project files; form validation added to CRUDButtons component for Edit form

--- records-api-front-end/src/components/CRUDButtons/CRUDButtons.jsx
+++ records-api-front-end/src/components/CRUDButtons/CRUDButtons.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 import Stack from "react-bootstrap/Stack";
@@ -8,11 +8,36 @@ import Form from "react-bootstrap/Form";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import StarRating from "../StarRating";
 import { RecordsAPI } from "../../apis/RecordsAPI";
+import * as formik from "formik";
+import * as yup from "yup";
 import "./styles.css";
 
 export const CRUDButtons = ({ record, setRecordDetails }) => {
-  const [showDelete, setShowDelete] = useState(false);
+  const { Formik } = formik;
+
+  const schema = yup.object().shape({
+    name: yup
+      .string()
+      .min(2, "Your name is too short.")
+      .max(100, "Your name is too long.")
+      .required("An item name is required."),
+    description: yup
+      .string()
+      .min(2, "Too short!")
+      .max(250, "Too Long")
+      .required("A description is required."),
+    price: yup
+      .string()
+      .required()
+      .matches(/^[0-9]*\.[0-9]{2}$/, "Invalid price"),
+  });
+
+  const [rating, setRating] = useState(record.rating);
   const [showEdit, setShowEdit] = useState(false);
+  const [showDelete, setShowDelete] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [showEditConfirmation, setShowEditConfirmation] = useState(false);
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
   const [values, setValues] = useState({
     id: record.id,
     name: record.name,
@@ -21,21 +46,27 @@ export const CRUDButtons = ({ record, setRecordDetails }) => {
     image: record.image,
   });
 
-  const [rating, setRating] = useState(record.rating);
-  const [showEditConfirmation, setShowEditConfirmation] = useState(false);
-  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+  useEffect(() => {
+    if (isSubmitting) {
+      RecordsAPI.update({ ...values, rating }).then(() => {
+        setRecordDetails({ ...values, rating });
+        setShowEdit(false);
+        setShowEditConfirmation(true);
+      });
+    }
+  }, [values, isSubmitting, rating, setRecordDetails]);
 
-  const onChangeHandler = (attribute) => {
-    return (event) => {
-      setValues({ ...values, [attribute]: event.target.value });
-    };
-  };
+  const navigate = useNavigate();
 
   const handleClose = () => {
     setShowEdit(false);
     setShowDelete(false);
     setShowEditConfirmation(false);
     setShowDeleteConfirmation(false);
+  };
+
+  const handleCloseEdit = () => {
+    setShowEdit(false);
   };
 
   const handleDeleteClose = () => {
@@ -46,21 +77,16 @@ export const CRUDButtons = ({ record, setRecordDetails }) => {
   const handleShowEdit = () => setShowEdit(true);
   const handleShowDelete = () => setShowDelete(true);
 
-  const navigate = useNavigate();
-
-  const handleEditClick = () => {
-    RecordsAPI.update({ ...values, rating }).then(() => {
-      setRecordDetails({ ...values, rating });
-      setShowEdit(false);
-      setShowEditConfirmation(true);
-    });
-  };
-
   const handleDeleteClick = (id) => {
     RecordsAPI.delete(id).then(() => {
       setShowDelete(false);
       setShowDeleteConfirmation(true);
     });
+  };
+
+  const editFormSubmit = (formValues) => {
+    setValues({ ...values, ...formValues });
+    setIsSubmitting(true);
   };
 
   return (
@@ -84,85 +110,129 @@ export const CRUDButtons = ({ record, setRecordDetails }) => {
         size="2x"
         fixedWidth
       />
-      {/* Edit Modal*/}
-      <Modal show={showEdit} onHide={handleClose}>
+      <Modal show={showEdit} onHide={handleCloseEdit}>
         <Modal.Header closeButton>
           <Modal.Title>Edit Item</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="editForm.ControlNameInput">
-              <Form.Label>Name</Form.Label>
-              <Form.Control
-                type="text"
-                value={values.name}
-                onChange={onChangeHandler("name")}
-                autoFocus
-              />
-            </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="editForm.ControlDescriptionInput"
-            >
-              <Form.Label>Description</Form.Label>
-              <Form.Control
-                as="textarea"
-                rows={3}
-                value={values.description}
-                onChange={onChangeHandler("description")}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="editForm.ControlPriceInput">
-              <Form.Label>Price</Form.Label>
-              <Form.Control
-                type="number"
-                value={values.price}
-                onChange={onChangeHandler("price")}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="addForm.ControlImageInput">
-              <Form.Label>Image</Form.Label>
-              <Form.Select
-                aria-label="add-form-image-select"
-                value={values.image}
-                onChange={onChangeHandler("image")}
-              >
-                <option>Open this select menu</option>
-                <option value="https:\/\/via.placeholder.com\/640x480.png\/0000ee?text=OptionOne">
-                  One
-                </option>
-                <option value="https:\/\/via.placeholder.com\/640x480.png\/0000ee?text=OptionTwo">
-                  Two
-                </option>
-                <option value="https:\/\/via.placeholder.com\/640x480.png\/0000ee?text=OptionThree">
-                  Three
-                </option>
-              </Form.Select>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="addForm.ControlRatingInput">
-              <Form.Label>Rating</Form.Label>
-              <StarRating
-                rating={rating}
-                size={30}
-                active={true}
-                setRating={setRating}
-              />
-            </Form.Group>
-          </Form>
+          <Formik
+            validationSchema={schema}
+            onSubmit={editFormSubmit}
+            initialValues={{
+              name: values.name,
+              description: values.description,
+              price: values.price,
+            }}
+          >
+            {({ handleSubmit, handleChange, values, touched, errors }) => (
+              <Form noValidate onSubmit={handleSubmit}>
+                <Form.Group
+                  className="mb-3"
+                  controlId="editForm.ControlNameInput"
+                >
+                  <Form.Label>Item Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="name"
+                    value={values.name}
+                    onChange={handleChange}
+                    isValid={touched.name && !errors.name}
+                    isInvalid={!!errors.name}
+                    autoFocus
+                  />
+                  <Form.Control.Feedback>
+                    Item name looks good!
+                  </Form.Control.Feedback>
+                  <Form.Control.Feedback type="invalid">
+                    {errors.name}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  controlId="editForm.ControlDescriptionInput"
+                >
+                  <Form.Label>Description</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={3}
+                    name="description"
+                    value={values.description}
+                    onChange={handleChange}
+                    isValid={touched.description && !errors.description}
+                    isInvalid={!!errors.description}
+                  />
+                  <Form.Control.Feedback>
+                    Description looks good!
+                  </Form.Control.Feedback>
+                  <Form.Control.Feedback type="invalid">
+                    {errors.description}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  controlId="editForm.ControlPriceInput"
+                >
+                  <Form.Label>Price</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="price"
+                    value={values.price}
+                    onChange={handleChange}
+                    isValid={touched.price && !errors.price}
+                    isInvalid={!!errors.price}
+                  />
+                  <Form.Control.Feedback>
+                    Price looks good!
+                  </Form.Control.Feedback>
+                  <Form.Control.Feedback type="invalid">
+                    {errors.price}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  controlId="addForm.ControlImageInput"
+                >
+                  <Form.Label>Image</Form.Label>
+                  <Form.Select
+                    aria-label="add-form-image-select"
+                    name="image"
+                    value={values.image}
+                    onChange={handleChange}
+                  >
+                    <option>Open this select menu</option>
+                    <option value="https:\/\/via.placeholder.com\/640x480.png\/0000ee?text=OptionOne">
+                      One
+                    </option>
+                    <option value="https:\/\/via.placeholder.com\/640x480.png\/0000ee?text=OptionTwo">
+                      Two
+                    </option>
+                    <option value="https:\/\/via.placeholder.com\/640x480.png\/0000ee?text=OptionThree">
+                      Three
+                    </option>
+                  </Form.Select>
+                </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  controlId="addForm.ControlRatingInput"
+                >
+                  <Form.Label>Rating</Form.Label>
+                  <StarRating
+                    rating={rating}
+                    size={30}
+                    active={true}
+                    setRating={setRating}
+                  />
+                </Form.Group>
+                <Button type="submit">Submit form</Button>
+              </Form>
+            )}
+          </Formik>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary" onClick={() => handleEditClick(record.id)}>
-            Save Changes
-          </Button>
-          <Button variant="danger" onClick={handleClose}>
-            Cancel
-          </Button>
-        </Modal.Footer>
       </Modal>
       {/* Delete Modal*/}
       <Modal show={showDelete} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Delete Item: {record.name}</Modal.Title>
+          <Modal.Title>Delete Item: {values.name}</Modal.Title>
         </Modal.Header>
         <Modal.Body>Are you sure you want to delete this item?</Modal.Body>
         <Modal.Footer>
